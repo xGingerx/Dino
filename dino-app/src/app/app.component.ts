a771d74b84c6746606d471f44c6065be749d36d8
xGingerx <david.kovacevic5.a@gmail.com>
2024-08-28 20:35:48 +0200

    navbar in home

--- dino-app/src/app/app.component.ts
+++ dino-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
