6d3cf8b0374f3db64ec34fac5685e1b869a7d9a6
xGingerx <david.kovacevic5.a@gmail.com>
2024-08-31 18:23:11 +0200

    Backend server added init

--- dino-app/src/app/about/about.component.ts
+++ dino-app/src/app/about/about.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
     selector: 'app-about',
     standalone: true,
-    imports:[RouterModule, RouterOutlet, NavbarComponent],
+    imports:[],
     templateUrl: './about.component.html',
     styleUrl: './about.component.css'
 })
--- dino-app/src/app/app.component.ts
+++ dino-app/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Auth, signInWithPopup, GoogleAuthProvider, User } from 'firebase/auth';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [CommonModule, RouterModule]  // Import RouterModule here
+  imports: [CommonModule, RouterModule, NavbarComponent]  // Import RouterModule here
 })
 export class AppComponent {
 }
