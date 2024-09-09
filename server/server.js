6788d107117a2cbc8b85e77c4a5b979b9f969f74
xGingerx <david.kovacevic5.a@gmail.com>
2024-09-09 10:00:28 +0200

    rename to reservation.component.ts

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const WebSocket = require('ws')
 const cors = require('cors');
 const ReservationController = require('./controller/ReservationController');
 const UserController = require('./controller/UserController')
@@ -19,3 +20,39 @@ app.use('/reservations', ReservationController)
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+const ws_port = 3002
+const DEFAULT_INTERVAL = 1000;
+
+const wss = new WebSocket.Server({ port: ws_port });
+
+wss.on("connection", (ws) => {
+    console.log("Client connected");
+    let timer = setInterval(generateRandomNumbers, DEFAULT_INTERVAL);
+
+    const generateRandomNumbers = () => {
+        if (ws.readyState === WebSocket.OPEN) {
+           const randomValue = Math.floor(Math.random() * 100);
+           ws.send(randomValue.toString());
+        }
+     };
+
+    ws.on("message", (message) => {
+        const newInterval = JSON.parse(message);
+        // Update the interval if received a new value from the client
+        console.log(`New interval: ${newInterval} second(s)`);
+        if (!isNaN(newInterval) && newInterval > 0) {
+           clearInterval(timer);
+           timer = setInterval(generateRandomNumbers, newInterval * 1_000);
+        }
+     });
+
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
+        clearInterval(timer);
+     });
+})
+
+
+console.log(`WebSocket server listening on port ${ws_port}`);
