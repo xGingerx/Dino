dc810cfe24a11f283f3c6bcaee777553205bcf79
xGingerx <david.kovacevic5.a@gmail.com>
2024-09-10 11:21:20 +0200

    port changed

--- dino-app/server.ts
+++ dino-app/server.ts
@@ -45,11 +45,11 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] ? +process.env['PORT'] : 4000;
 
   // Start up the Node server
   const server = app();
-  server.listen(port, () => {
+  server.listen(port, '0.0.0.0',() => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
